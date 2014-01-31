0a558ffc2cbce24c58240c227fb5fd3abaab3b1e
Kyle Wilcox <wilcox32ahs@gmail.com>
2014-01-31 14:26:26 -0500

    Init Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoogleHTML::Application.config.secret_key_base = '6353fcb1f76cb34b6524b0d5d11fbaac32572f1b31e19d0431339a671331f35b7b012eb42e6e085f45f8b06bffe5245b850fc331d975067c02ef6f678e271b09'
